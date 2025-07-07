87340eae171a73071fe77012fcf636b95d1cf161
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2025-07-07 14:00:18 +0100

    Update notifier.py

--- ai-assistant/app/notifier.py
+++ ai-assistant/app/notifier.py
@@ -3,8 +3,8 @@ import requests
 import os
 
 def notify_user(message: str):
-    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    bot_token = os.getenv("7868677366:AAGcZrxUrxijX_HYLJlQCzOS35a4OyScUII")
+    chat_id = os.getenv("Fauwaz_a")
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     try:
