58661e5e40cd008a03d2b532a64faa791e37527b
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2025-07-08 00:47:40 +0100

    Update notifier.py

--- app/notifier.py
+++ app/notifier.py
@@ -4,7 +4,7 @@ import os
 
 def notify_user(message: str):
     bot_token = os.getenv("7868677366:AAGcZrxUrxijX_HYLJlQCzOS35a4OyScUII")
-    chat_id = os.getenv("Fauwaz_a")
+    chat_id = os.getenv("1245158718")
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     try:
