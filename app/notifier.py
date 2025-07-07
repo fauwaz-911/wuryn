24868839f559b4f347a26e66a91df512c0829065
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2025-07-08 00:57:25 +0100

    Update notifier.py

--- app/notifier.py
+++ app/notifier.py
@@ -1,13 +1,19 @@
-
+'''
+7868677366:AAGcZrxUrxijX_HYLJlQCzOS35a4OyScUII
+1245158718
+'''
 import requests
-import os
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_USER_ID
 
 def notify_user(message: str):
-    bot_token = os.getenv("7868677366:AAGcZrxUrxijX_HYLJlQCzOS35a4OyScUII")
-    chat_id = os.getenv("1245158718")
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_USER_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
     try:
-        requests.post(url, data=data)
-    except Exception as e:
-        print(f"[Notification Failed] {e}")
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print("❌ Telegram error:", e)
