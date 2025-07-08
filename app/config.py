825a273e73d259592cecc36f217969248c1f9539
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2025-07-08 09:36:57 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 
 # Optional: config loader if needed in future
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Load environment variables from .env (for local use)
-load_dotenv()
+#load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("7868677366:AAGcZrxUrxijX_HYLJlQCzOS35a4OyScUII")
 TELEGRAM_USER_ID = os.getenv("1245158718")
