56d15b40dac7e65363cdde0cf0439656061d63a9
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2025-07-08 01:03:37 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 # Load environment variables from .env (for local use)
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("7868677366:AAGcZrxUrxijX_HYLJlQCzOS35a4OyScUII")
+TELEGRAM_USER_ID = os.getenv("1245158718")
